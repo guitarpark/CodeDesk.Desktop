579ce9662f70cc8a46107429ccc405642c4e7e91
guitarpark <11057599@qq.com>
2023-11-14 01:49:21 +0000

    删除文件 CodeDesk.Website

--- CodeDesk.Website/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CodeDesk.Website.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
